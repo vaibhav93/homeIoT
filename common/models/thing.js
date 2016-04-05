c3dc783bfbef143f9fbaeb1dbec6bb6688130296
Vaibhav <vaibhavbansal1993@gmail.com>
2016-04-05 20:21:46 +0530

    api bug

--- common/models/thing.js
+++ common/models/thing.js
@@ -6,7 +6,7 @@ module.exports = function(Thing) {
         if (ctx.args.data.power) {
             console.log(' Power changed to: 1');
             app.client.publish('home', "1");
-        } else if (!ctx.args.data.power) {
+        } else {
             console.log(' Power changed to: 0');
             app.client.publish('home', "0");
         }
