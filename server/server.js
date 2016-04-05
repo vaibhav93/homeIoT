e3d116a8c849ab22544d145482f91bba1121bb51
Vaibhav <vaibhavbansal1993@gmail.com>
2016-04-05 20:47:46 +0530

    moment

--- server/server.js
+++ server/server.js
@@ -73,7 +73,7 @@ setInterval(function() {
             }
         }
     })
-}, 300000)
+}, 900000)
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname, function(err) {
