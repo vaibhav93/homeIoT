cf169bef88c8845db026eb0e665997990b9341b6
Vaibhav <vaibhavbansal1993@gmail.com>
2016-04-11 01:47:48 +0530

    save and clear button

--- client/panel/js/controllers/page1Ctrl.js
+++ client/panel/js/controllers/page1Ctrl.js
@@ -1,21 +1,26 @@
 app.controller("page1Ctrl", function($scope, Thing) {
     $scope.thing = {};
-    Thing.findOne({
-        filter: {
-            where: {
-                mqtt_client_id: "Cooler_1"
+
+    $scope.reload = function() {
+        Thing.findOne({
+            filter: {
+                where: {
+                    mqtt_client_id: "Cooler_1"
+                }
             }
-        }
-    }, function(thing) {
-        $scope.thing = thing;
-    })
-    $scope.message = "hi I am new angular app";
-    $scope.persons = [{
-        fName: "Vaibhav",
-        lName: "Gupta"
-    }, {
-        fName: "Shubhangi",
-        lName: "Gupta"
-    }];
+        }, function(thing) {
+            $scope.thing = thing;
+        });
+    }
+    $scope.reload();
+    $scope.saveChanges = function() {
+        Thing.prototype$updateAttributes({
+            id: $scope.thing.id
+        }, $scope.thing, function(success) {
+            console.log(success)
+        }, function(err) {
+            console.log(err)
+        });
+    }
 
 });
