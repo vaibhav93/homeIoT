526ef5e2455a721864a0b03b9035487135c15c66
Vaibhav <vaibhavbansal1993@gmail.com>
2016-04-11 01:40:33 +0530

    lb service

--- client/panel/js/app.js
+++ client/panel/js/app.js
@@ -1,1 +1,1 @@
-var app=angular.module("test",['ngMaterial']);
+var app = angular.module("test", ['ngMaterial', 'lbServices']);
--- client/panel/js/controllers/page1Ctrl.js
+++ client/panel/js/controllers/page1Ctrl.js
@@ -1,5 +1,14 @@
-app.controller("page1Ctrl", function ($scope) {
-
+app.controller("page1Ctrl", function($scope, Thing) {
+    $scope.thing = {};
+    Thing.findOne({
+        filter: {
+            where: {
+                mqtt_client_id: "Cooler_1"
+            }
+        }
+    }, function(thing) {
+        $scope.thing = thing;
+    })
     $scope.message = "hi I am new angular app";
     $scope.persons = [{
         fName: "Vaibhav",
