b2391a4111431955e8096f018d8e3d9c7c4d5409
Vaibhav <vaibhavbansal1993@gmail.com>
2016-04-11 01:53:28 +0530

    Timer binding

--- common/models/thing.js
+++ common/models/thing.js
@@ -6,10 +6,12 @@ module.exports = function(Thing) {
         if (ctx.args.data.power) {
             console.log(' Power changed to: 1');
             app.client.publish('home', "1");
-        } else {
+        } else if (!ctx.args.data.power) {
             console.log(' Power changed to: 0');
             app.client.publish('home', "0");
         }
+
+
         next();
     });
 }
