6bf18d4bc90cd64bfa4c7f2c30f19dda127bf08d
Vaibhav <vaibhavbansal1993@gmail.com>
2016-04-11 02:26:42 +0530

    timer

--- common/models/thing.js
+++ common/models/thing.js
@@ -1,6 +1,44 @@
 var app = require('../../server/server');
 module.exports = function(Thing) {
+    var clearAllIntervals = function() {
+        for (var i = 1; i < 99999; i++)
+            window.clearInterval(i);
+    }
+    var onInterval = function(mqttID, onTime, offTime) {
+        //on
+        thingSetPower(mqttID, true);
+        var me = setTimeout(function(mqttID, offTime) {
+            offInterval(mqttID, offTime);
+        }, onTime, mqttID, offTime);
+    };
 
+    var offInterval = function(mqttID, offTime, onTime) {
+        //off
+        thingSetPower(mqttID, false);
+        var me = setTimeout(function(mqttID, onTime) {
+            onInterval(mqttID, onTime);
+        }, offTime, mqttID, onTime)
+    };
+    var thingSetPower = function(mqttID, power) {
+        app.models.Thing.findOne({
+            where: {
+                mqtt_client_id: mqttID
+            }
+        }, function(err, thing) {
+            if (err || !thing)
+                console.log(err);
+            else {
+                thing.updateAttributes({
+                    power: power
+                }, function(err, updatedThing) {
+                    if (!power)
+                        app.client.publish('home', "0");
+                    else
+                        app.client.publish('home', "1");
+                })
+            }
+        })
+    }
     Thing.beforeRemote('prototype.updateAttributes', function(ctx, result, next) {
         console.log('Change power from api.new Power:' + ctx.args.data.power);
         if (ctx.args.data.power) {
@@ -9,6 +47,12 @@ module.exports = function(Thing) {
         } else if (!ctx.args.data.power) {
             console.log(' Power changed to: 0');
             app.client.publish('home', "0");
+        }
+        if (ctx.args.data.timer.status) {
+            //switch ON
+            onInterval(ctx.args.data.mqtt_client_id, ctx.args.data.timer.on, ctx.args.data.timer.off);
+        } else if (!ctx.args.data.timer.status) {
+            clearAllIntervals();
         }
 
 
